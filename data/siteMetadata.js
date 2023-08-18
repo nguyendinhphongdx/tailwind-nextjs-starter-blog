75d288c247dca6978c4a6567509fec71ea9f1be5
nguyendinhphongdx <phongnguyendx@gmail.com>
2023-08-19 00:09:12 +0700

    add

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'IT Blog',
+  author: 'Nguyễn Đình Phong',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
